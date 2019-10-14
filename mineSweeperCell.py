5e017e2121f17a85bf3591cdbf756f47341b53bd
SydanJainen <34268197+SydanJainen@users.noreply.github.com>
2019-10-14 17:47:11 +0200

    Delete mineSweeperCell.py

--- mineSweeperCell.py
+++
@@ -1,9 +0,0 @@
-
-
-class cell:
-
-    def __init__(self, x, y, content):
-        self.x = x
-        self.y = y
-        # 0..8 — numeric content, also FLAGGED and UNKNOWN constants
-        self.content = content
